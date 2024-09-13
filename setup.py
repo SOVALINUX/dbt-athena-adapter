4773d527b16248ebd8d9572d6635c4cb40b566cf
Egor Tananaiko <Egor_Tananaiko@epam.com>
2024-09-13 14:22:57 +0200

    Bump version: 1.8.0 → 1.8.1

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
         "dbt-common>=1.0.0b2,<2.0",
         "dbt-adapters>=1.0.0b2,<2.0",
         # add dbt-core to ensure backwards compatibility of installation, this is not a functional dependency
-        "dbt-core>=1.8.0",
+        "dbt-core>=1.8.1",
         "boto3>=1.28",
         "boto3-stubs[athena,glue,lakeformation,sts]>=1.28",
         "pyathena>=2.25,<4.0",
