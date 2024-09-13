69bc70bc4c84d3419d85951ad2a73317de3792cc
Egor Tananaiko <Egor_Tananaiko@epam.com>
2024-09-13 14:02:40 +0200

    rewrite conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,24 @@
 import os
 from io import StringIO
+from unittest.mock import MagicMock, patch
 
+import boto3
 import pytest
+from dbt_common.events import get_event_manager
+from dbt_common.events.base_types import EventLevel
+from dbt_common.events.logger import LineFormat, LoggerConfig, NoFilter
 
-from dbt.events.base_types import EventLevel
-from dbt.events.eventmgr import LineFormat, NoFilter
-from dbt.events.functions import EVENT_MANAGER, _get_stdout_config
+from dbt.adapters.athena import connections
+from dbt.adapters.athena.connections import AthenaCredentials
+
+from .unit.constants import (
+    ATHENA_WORKGROUP,
+    AWS_REGION,
+    DATA_CATALOG_NAME,
+    DATABASE_NAME,
+    S3_STAGING_DIR,
+    SPARK_WORKGROUP,
+)
 
 # Import the functional fixtures as a plugin
 # Note: fixtures with session scope need to be local
@@ -25,9 +38,10 @@ def dbt_profile_target():
         "region_name": os.getenv("DBT_TEST_ATHENA_REGION_NAME"),
         "threads": int(os.getenv("DBT_TEST_ATHENA_THREADS", "1")),
         "poll_interval": float(os.getenv("DBT_TEST_ATHENA_POLL_INTERVAL", "1.0")),
-        "num_retries": 0,
+        "num_retries": int(os.getenv("DBT_TEST_ATHENA_NUM_RETRIES", "2")),
         "work_group": os.getenv("DBT_TEST_ATHENA_WORK_GROUP"),
         "aws_profile_name": os.getenv("DBT_TEST_ATHENA_AWS_PROFILE_NAME") or None,
+        "spark_work_group": os.getenv("DBT_TEST_ATHENA_SPARK_WORK_GROUP"),
     }
 
 
@@ -42,15 +56,34 @@ def dbt_debug_caplog() -> StringIO:
 
 
 def _setup_custom_caplog(name: str, level: EventLevel):
-    capture_config = _get_stdout_config(
-        line_format=LineFormat.PlainText,
+    string_buf = StringIO()
+    capture_config = LoggerConfig(
+        name=name,
         level=level,
         use_colors=False,
-        log_cache_events=True,
+        line_format=LineFormat.PlainText,
+        filter=NoFilter,
+        output_stream=string_buf,
     )
-    capture_config.name = name
-    capture_config.filter = NoFilter
-    string_buf = StringIO()
-    capture_config.output_stream = string_buf
-    EVENT_MANAGER.add_logger(capture_config)
+    event_manager = get_event_manager()
+    event_manager.add_logger(capture_config)
     return string_buf
+
+
+@pytest.fixture(scope="class")
+def athena_client():
+    with patch.object(boto3.session.Session, "client", return_value=MagicMock()) as mock_athena_client:
+        return mock_athena_client
+
+
+@patch.object(connections, "AthenaCredentials")
+@pytest.fixture(scope="class")
+def athena_credentials():
+    return AthenaCredentials(
+        database=DATA_CATALOG_NAME,
+        schema=DATABASE_NAME,
+        s3_staging_dir=S3_STAGING_DIR,
+        region_name=AWS_REGION,
+        work_group=ATHENA_WORKGROUP,
+        spark_work_group=SPARK_WORKGROUP,
+    )
